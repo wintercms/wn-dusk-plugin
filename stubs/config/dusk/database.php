a871a95c0664078cb652196ca755750753b92b6e
Ben Thomson <git@alfreido.com>
2024-04-11 10:27:42 +0800

    Fix stub configs to prevent SMTP connection

--- stubs/config/dusk/database.php
+++ stubs/config/dusk/database.php
@@ -7,37 +7,6 @@ return [
             'driver'   => 'sqlite',
             'database' => env('DB_DATABASE', 'storage/dusk.sqlite'),
         ],
-        'mysql' => [
-            'driver'     => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', 3306),
-            'database' => env('DB_DATABASE', 'database'),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
-        ],
-        'pgsql' => [
-            'driver'   => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', 5432),
-            'database' => env('DB_DATABASE', 'database'),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
-        ],
-        'sqlsrv' => [
-            'driver'   => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', 5432),
-            'database' => env('DB_DATABASE', 'database'),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
-        ],
-    ],
-    'redis' => [
-        'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', ''),
-            'port' => env('REDIS_PORT', 6379),
-        ],
     ],
     'useConfigForTesting' => env('DB_USE_CONFIG_FOR_TESTING', false),
 ];
--- stubs/config/dusk/mail.php
+++ stubs/config/dusk/mail.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'driver' => env('MAIL_DRIVER', 'array'),
-    'host' => env('MAIL_HOST', ''),
-    'port' => env('MAIL_PORT'),
-    'encryption' => env('MAIL_ENCRYPTION'),
-    'username' => env('MAIL_USERNAME', ''),
-    'password' => env('MAIL_PASSWORD', ''),
+    'default' => env('MAIL_MAILER', 'array'),
+    'mailers' => [
+        'array' => [
+            'transport' => 'array',
+        ],
+    ],
 ];
